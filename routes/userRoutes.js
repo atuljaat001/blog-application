5fcd06523c5fd0fe735d6a5dff3b3b452f5dc724
Atul Saharan <saharanatul1603@gmail.com>
2024-07-26 18:42:50 +0530

    little changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,11 +17,12 @@ const route = Router();
 
 route.route("/login").get(handleRenderLogin).post(handleLogin);
 route.route("/signup").get(handleRenderLogin).post(handleSignup);
-route.use(checkLogin);
-route.get("/home", handleRenderHome);
 route.route("/forgot-password").get(handleforget).post(handleForgetPassword);
 route.post("/verify-otp", handleVerificationOfOtp);
 route.post("/create-new-password", handlePasswordChange);
+route.use(checkLogin);
+route.get("/home", handleRenderHome);
+
 route.get("/logout", handleLogout);
 
 route.route("/blog").get(handleRenderBlog).post(handleBlogPost);
